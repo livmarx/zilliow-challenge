8b3c8626b0a23e2ad53e749e2fd24ae03071ace3
Liv Marx <oehrenmarks@gmail.com>
2018-12-10 10:22:29 -0500

    rendering hello

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express(); // object with methods; One method is router; express creates a server; app allows us to access the server; app.listen hooks up our server;
 
 const morgan = require('morgan'); //app.use(...) morgan says console.log(what app.use does);
-const { db, List, ToDo, User } = require('./backend/db/index');
+// const { db, List, ToDo, User } = require('./backend/db/index');
 
 // Logger:
 app.use(morgan('dev')); // implies '/' ('/', morgan('dev'));
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 
 // Routes
-app.use('/api', require('./backend/routes/index'));
+// app.use('/api', require('./backend/routes/index'));
 
 // app.get('/', function(req, res, next) {
 //   try {
@@ -31,7 +31,7 @@ app.use('/api', require('./backend/routes/index'));
 //   }
 // });
 
-const PORT = 8080;
+const PORT = 8888;
 
 const init = async function() {
   // await db.sync();
--- client/components/Main.js
+++ client/components/Main.js
@@ -1,27 +1,24 @@
 import React from 'react';
-import SingleTodo from './SingleTodo';
 import axios from 'axios';
 
 export default class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      users: [],
+      events: [],
     };
   }
 
   async componentDidMount() {
     const res = await axios.get('/api/allUsers');
-    const allUsers = res.data;
-    this.setState({ users: allUsers });
+    const allEvents = res.data;
+    this.setState({ events: allEvents });
   }
 
   render() {
-    console.log('this.state.users: ', this.state.users);
     return (
       <div>
         <h1>LIST</h1>
-        <SingleTodo />
       </div>
     );
   }
