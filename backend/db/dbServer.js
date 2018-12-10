d869e05fe917fee27b73d26bb73bbff26c86247c
Liv Marx <oehrenmarks@gmail.com>
2018-12-10 10:10:56 -0500

    starting

--- backend/db/dbServer.js
+++
@@ -1,6 +0,0 @@
-const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost:5432/todotoday', {
-  logging: false,
-});
-
-module.exports = { db };
--- backend/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.use('/allUsers', require('./userRoutes'));
-router.use('/allLists', require('./listRoutes'));
-router.use('/allTodos', require('./todoRoutes'));
-
-module.exports = router;
