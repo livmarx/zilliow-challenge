b1351c90404deeb105e730d67e0a8426bf4c0834
Liv Marx <oehrenmarks@gmail.com>
2019-02-12 11:27:08 -0500

    debug net::ERR_ABORTED 404 (Not Found)

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const path = require('path');
 const app = express(); // object with methods; One method is router; express creates a server; app allows us to access the server; app.listen hooks up our server;
 
 const morgan = require('morgan'); //app.use(...) morgan says console.log(what app.use does);
-// const { db, List, ToDo, User } = require('./backend/db/index');
 
 // Logger:
 app.use(morgan('dev')); // implies '/' ('/', morgan('dev'));
@@ -15,8 +14,14 @@ app.use(express.json());
 // public:
 console.log('dirname', __dirname);
 app.use(express.static(__dirname + '/public'));
+// app.get('*', function(req, res) {
+//   res.send('root');
+// });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+}); // Send index.html for any other requests
 
-const PORT = process.env.PORT || 8800;
+const PORT = process.env.PORT || 3000;
 
 const init = async function() {
   // await db.sync();
