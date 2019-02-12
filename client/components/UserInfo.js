038b9991af68513bda8eb1e4e65cccdd94b9d832
Liv Marx <oehrenmarks@gmail.com>
2019-02-12 11:06:35 -0500

    boom boom

--- client/components/UserInfo.js
+++ client/components/UserInfo.js
@@ -6,7 +6,9 @@ export default function UserInfo(props) {
       <img src={props.user.avatar_url} />
       <div className="user-names">
         <h2>{props.user.name}</h2>
-        <h3>GitHub Username: {props.user.login}</h3>
+        <h3>Username: {props.user.login}</h3>
+        <p> Contact Info: {props.user.email}</p>
+        <p> GitHub URL: {props.user.html_url}</p>
       </div>
     </div>
   );
